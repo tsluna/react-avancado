2374e9d9623c4fcf0e9e073c31885091e4645999
Thiago Luna <thiago.luna.silva@gmail.com>
2020-08-25 10:58:38 -0300

    [master] - update version storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,12 +1,13 @@
 import { text, withKnobs } from '@storybook/addon-knobs';
+import { Meta, Story } from '@storybook/react/types-6-0';
 import Main from '.';
 export default {
   component: Main,
   title: 'Main',
   decorators: [withKnobs]
-};
+} as Meta;
 
-export const Basic = () => (
+export const Basic: Story = () => (
   <Main
     title={text('Title', 'React Avançado')}
     descrition={text(
