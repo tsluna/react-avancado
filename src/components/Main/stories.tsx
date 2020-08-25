ee4ebe39424963fd7fdcf6aafe688869b7f7bbc1
Thiago Luna <thiago.luna.silva@gmail.com>
2020-08-25 11:19:54 -0300

    [master] - Add storybook/addon-essentials

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,18 +1,13 @@
-import { text, withKnobs } from '@storybook/addon-knobs';
 import { Meta, Story } from '@storybook/react/types-6-0';
 import Main from '.';
 export default {
   component: Main,
   title: 'Main',
-  decorators: [withKnobs]
+  args: {
+    title: 'Luna title',
+    description: 'Minha descricao padrao'
+  }
 } as Meta;
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    descrition={text(
-      'Description',
-      'TypeScript, ReactJs, NextJs e Styled Components'
-    )}
-  />
-);
+export const Basic: Story = (args) => <Main {...args} />;
+export const Default: Story = (args) => <Main {...args} />;
